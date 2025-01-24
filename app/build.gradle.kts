6eddbfc17c96518eb290682b8ecbb1b3683d56c1
W.A.Punsisi Yemani Perera <wapunsisiyemani18@gmail.com>
2025-01-25 00:01:01 +0530

    error fixed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,11 +40,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-
-
-
-
-
     // Glide dependencies
     implementation(libs.glide)
 
