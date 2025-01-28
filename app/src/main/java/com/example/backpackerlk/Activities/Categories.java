23a21b2bd48aede2f556d2a87b8308762b73a478
W.A.Punsisi Yemani Perera <wapunsisiyemani18@gmail.com>
2025-01-28 11:03:52 +0530

    I have created who are you ui and create the navigation

--- app/src/main/java/com/example/backpackerlk/Activities/Categories.java
+++ app/src/main/java/com/example/backpackerlk/Activities/Categories.java
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import com.example.backpackerlk.R;
+import com.example.backpackerlk.UserProfile;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Categories extends AppCompatActivity {
 
@@ -18,6 +20,33 @@ public class Categories extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+
+        //navigation
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.nav_categories);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.nav_home) {
+                startActivity(new Intent(getApplicationContext(), Home.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.nav_categories){
+                startActivity(new Intent(getApplicationContext(), Categories.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            } else if (itemId == R.id.nav_profile) {
+                startActivity(new Intent(getApplicationContext(), WhoAreYou.class));
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+                finish();
+                return true;
+            }
+
+            return false;
+        });
 
         // Initialize the CardViews
         safariCard = findViewById(R.id.safari);
